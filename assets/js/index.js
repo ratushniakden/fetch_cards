3eda7c0fadd180cacc605556557acad47a79d959
ratushniakden <ratushniak.do@gmail.com>
2020-09-20 19:37:24 +0300

    fix bugs

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,26 +6,81 @@ fetch("/users.json")
     const ul = document.getElementById("ul");
 
     const usersList = users.map((user) => {
-      const li = document.createElement("li");
-      const img = document.createElement("img");
-      const h1 = document.createElement("h1");
-      const p = document.createElement("p");
-      const button = document.createElement("button");
+      if (user.id < 100) {
+        const liOptions = {
+          className: "userCardContainer",
+        };
 
-      li.setAttribute("class", "userCardContainer");
-      img.setAttribute("src", user.profilePicture);
+        const imgOptions = {
+          src: user.profilePicture ?? "/assets/image/defaultUser.png",
+        };
 
-      h1.textContent = `${user.firstName} ${user.lastName}`;
-      p.textContent = randomProfession();
-      button.textContent = "Connect";
+        const headingOptions = {
+          tagName: "h1",
+          textContent: `${user.firstName} ${user.lastName}`,
+        };
 
-      li.append(img, h1, p, button);
+        const paragraphOptions = {
+          textContent: randomProfession(),
+        };
 
-      return li;
+        const buttonOptions = {
+          textContent: "Connect",
+        };
+
+        const li = createLi(liOptions);
+        const img = createImage(imgOptions);
+        const h1 = createHeading(headingOptions);
+        const p = createParagraph(paragraphOptions);
+        const button = createButton(buttonOptions);
+
+        li.append(img, h1, p, button);
+
+        return li;
+      } else {
+        return null;
+      }
     });
-    ul.append(...usersList);
+    ul.append(
+      ...usersList.filter(function (e) {
+        return e != null;
+      })
+    );
   })
   .catch(console.error);
+
+function createLi({ className = "li" } = options) {
+  const li = document.createElement("li");
+  li.setAttribute("class", className);
+  return li;
+}
+
+function createImage({ src = "/assets/image/defaultUser.png" } = options) {
+  const img = document.createElement("img");
+  img.src = src;
+  return img;
+}
+
+function createHeading({
+  tagName = "h1",
+  textContent = "User Name",
+} = options) {
+  const headingElement = document.createElement(tagName);
+  headingElement.textContent = textContent;
+  return headingElement;
+}
+
+function createParagraph({ textContent = "Profession" } = options) {
+  const pElement = document.createElement("p");
+  pElement.textContent = textContent;
+  return pElement;
+}
+
+function createButton({ textContent = "More" } = options) {
+  const btn = document.createElement("button");
+  btn.textContent = textContent;
+  return btn;
+}
 
 function randomProfession() {
   let prof = [
