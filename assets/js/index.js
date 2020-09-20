7e342fb3f723bc201a5d8c442a166ba6c6bd1817
ratushniakden <ratushniak.do@gmail.com>
2020-09-20 18:26:21 +0300

    remove extra div

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,21 +7,19 @@ fetch("/users.json")
 
     const usersList = users.map((user) => {
       const li = document.createElement("li");
-      const div = document.createElement("div");
       const img = document.createElement("img");
       const h1 = document.createElement("h1");
       const p = document.createElement("p");
       const button = document.createElement("button");
 
-      div.setAttribute("class", "userCardContainer");
+      li.setAttribute("class", "userCardContainer");
       img.setAttribute("src", user.profilePicture);
 
       h1.textContent = `${user.firstName} ${user.lastName}`;
       p.textContent = randomProfession();
       button.textContent = "Connect";
 
-      div.append(img, h1, p, button);
-      li.append(div);
+      li.append(img, h1, p, button);
 
       return li;
     });
